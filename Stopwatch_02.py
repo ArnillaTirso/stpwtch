7cbd7e5cb684e0155943a5b28125b9dc4e85d8f0
RalphAndrew <106044455+RalphAndrew@users.noreply.github.com>
2022-05-25 14:27:33 +0800

    Update Stopwatch_02.py

--- Stopwatch_02.py
+++ Stopwatch_02.py
@@ -16,3 +16,25 @@ class StopWatch(Frame):
         self.laps = []
         self.lapmod2 = 0
         self.today = time.strftime("%d %b %Y %H-%M-%S", time.localtime())
+        
+    def makeWidgets(self):                         
+    """ Make the time label. """
+    l1 = Label(self, text='STOPWATCH')
+    l1.pack(fill=X, expand=NO, pady=1, padx=2)
+
+    self.e = Entry(self)
+    self.e.pack(pady=2, padx=2)
+    
+    l = Label(self, textvariable=self.timestr)
+    self._setTime(self._elapsedtime)
+    l.pack(fill=X, expand=NO, pady=3, padx=2)
+
+    l2 = Label(self, text='----Laps----')
+    l2.pack(fill=X, expand=NO, pady=4, padx=2)
+
+    scrollbar = Scrollbar(self, orient=VERTICAL)
+    self.m = Listbox(self,selectmode=EXTENDED, height = 5,
+                     yscrollcommand=scrollbar.set)
+    self.m.pack(side=LEFT, fill=BOTH, expand=1, pady=5, padx=2)
+    scrollbar.config(command=self.m.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
