04af42cb8064dc304c0219552cae396e04d19ca5
061321 <106162183+061321@users.noreply.github.com>
2022-05-26 11:09:26 +0800

    Update Stopwatch_02.py

--- Stopwatch_02.py
+++ Stopwatch_02.py
@@ -38,3 +38,15 @@ class StopWatch(Frame):
     self.m.pack(side=LEFT, fill=BOTH, expand=1, pady=5, padx=2)
     scrollbar.config(command=self.m.yview)
     scrollbar.pack(side=RIGHT, fill=Y)
+    def _update(self): 
+        """ Update the label with elapsed time. """
+        self._elapsedtime = time.time() - self._start
+        self._setTime(self._elapsedtime)
+        self._timer = self.after(50, self._update)
+    
+    def _setTime(self, elap):
+        """ Set the time string to Minutes:Seconds:Hundreths """
+        minutes = int(elap/60)
+        seconds = int(elap - minutes*60.0)
+        hseconds = int((elap - minutes*60.0 - seconds)*100)                
+        self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
