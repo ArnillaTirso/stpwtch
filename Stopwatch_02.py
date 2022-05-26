57d954750aff3657b28775510a2beb3331412ff4
Qamidon <86446725+Qamidon@users.noreply.github.com>
2022-05-26 11:16:56 +0800

    Update Stopwatch_02.py

--- Stopwatch_02.py
+++ Stopwatch_02.py
@@ -51,3 +51,16 @@ class StopWatch(Frame):
         seconds = int(elap - minutes*60.0)
         hseconds = int((elap - minutes*60.0 - seconds)*100)                
         self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
+        
+        def _update(self): 
+        """ Update the label with elapsed time. """
+        self._elapsedtime = time.time() - self._start
+        self._setTime(self._elapsedtime)
+        self._timer = self.after(50, self._update)
+    
+    def _setTime(self, elap):
+        """ Set the time string to Minutes:Seconds:Hundreths """
+        minutes = int(elap/60)
+        seconds = int(elap - minutes*60.0)
+        hseconds = int((elap - minutes*60.0 - seconds)*100)                
+        self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
